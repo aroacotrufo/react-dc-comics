d78a0ff71e09216fed13b89051ebdbb5f3920bdf
Aroa Cotrufo <aroa.c97@gmail.com>
2026-03-06 15:54:12 +0100

    setup repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AppHeader from './components/AppHeader'
+import AppMain from './components/AppMain'
+import AppFooter from './components/AppMain'
+
+function App() {
+
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
